cc01100fe5e1f058acee190d0de05f74f70441d6
Kotaro Okuya <ktr.okuya@karabiner.tech>
2018-01-16 01:15:53 +0900

    Routing animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component'
+import { ArticleComponent } from './article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'articles', component: ArticlesComponent, data:{depth: 1} },
+    { path: 'article', component: ArticleComponent, data:{depth: 2}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from './menu/menu.component'
+import { ArticlesComponent } from './articles/articles.component'
+import { trigger, transition, group, query, style, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+      trigger('routeAnimation', [
+        transition('1 => 2', [
+            style({height: '!'}),
+            query(':enter', style({ transform: 'translateX(100%)'})),
+            query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+            // animate the leave page away
+            group([
+                query(':leave', [
+                    animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(-100%)' })),
+                ]),
+                // and now reveal the enter
+                query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+            ]),
+          ]),
+        transition('2 => 1', [
+            style({ height: '!' }),
+            query(':enter', style({ transform: 'translateX(-100%)' })),
+            query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+            // animate the leave page away
+            group([
+                query(':leave', [
+                    animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(100%)' })),
+                ]),
+                // and now reveal the enter
+                query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
+            ]),
+        ]),
+      ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+
+    
+
+    getDepth(outlet){
+        console.log(outlet.activatedRouteData['depth'])
+        return outlet.activatedRouteData['depth']
+    }
 }
